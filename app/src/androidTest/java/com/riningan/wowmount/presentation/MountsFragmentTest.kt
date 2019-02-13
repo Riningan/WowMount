cfa452993a4d7f0d79d3fe6379a1d0008daef836
Riningan <Riningan@gmail.com>
2019-02-14 01:26:58 +0500

    develop

--- app/src/androidTest/java/com/riningan/wowmount/presentation/MountsFragmentTest.kt
+++ app/src/androidTest/java/com/riningan/wowmount/presentation/MountsFragmentTest.kt
@@ -1,17 +1,22 @@
 package com.riningan.wowmount.presentation
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.UiController
+import android.support.test.espresso.ViewAction
 import android.support.test.espresso.ViewAssertion
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.contrib.ViewPagerActions.scrollLeft
 import android.support.test.espresso.contrib.ViewPagerActions.scrollRight
+import android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.runner.AndroidJUnit4
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.riningan.frarg.processor.MountsFragmentArgs
 import com.riningan.wowmount.R
 import com.riningan.wowmount.dispatcher.Error401Dispatcher
@@ -22,6 +27,7 @@ import com.riningan.wowmount.presentation.ui.mounts.MountsFragment
 import com.riningan.wowmount.rule.AppRule
 import okhttp3.mockwebserver.MockWebServer
 import org.awaitility.Awaitility.await
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
 import org.junit.After
@@ -377,6 +383,50 @@ class MountsFragmentTest {
                 }
     }
 
+    @Test
+    fun swipeRefresh() {
+        mWebServer.setDispatcher(ErrorDispatcher())
+
+        mAppRule.launch(MountsFragment::class.java, MountsFragmentArgs(true))
+
+        await().atMost(10, TimeUnit.SECONDS)
+                .ignoreExceptions()
+                .untilAsserted {
+                    onView(withId(R.id.crdlMounts)).check(matches(isDisplayed()))
+                }
+
+        await().atMost(5, TimeUnit.SECONDS)
+                .ignoreExceptions()
+                .untilAsserted {
+                    onView(allOf(isDisplayed(), withId(R.id.rvMounts)))
+                            .check(recyclerViewItemCountAssertion(4))
+                }
+
+        await().atMost(5, TimeUnit.SECONDS)
+                .ignoreExceptions()
+                .untilAsserted {
+                    onView(withId(android.support.design.R.id.snackbar_text)).check(matches(isDisplayed()))
+                }
+
+        mWebServer.setDispatcher(RequestDispatcher())
+
+        onView(withId(R.id.srlMounts)).perform(setRefreshing())
+        // onView(..).perform(..) does not work when views are animated.
+        // Therefore this is using a posted task to turn off refreshing.
+        val swipeRefreshLayout = mAppRule.getActivity().findViewById<SwipeRefreshLayout>(R.id.srlMounts)
+        swipeRefreshLayout.
+//        swipeRefreshLayout.handler.post {
+//            swipeRefreshLayout.isRefreshing = false
+//        }
+
+        await().atMost(150, TimeUnit.SECONDS)
+                .ignoreExceptions()
+                .untilAsserted {
+                    onView(allOf(isDisplayed(), withId(R.id.rvMounts)))
+                            .check(recyclerViewItemCountAssertion(711))
+                }
+    }
+
 
     private fun recyclerViewItemCountAssertion(expectedCount: Int) =
             ViewAssertion { view, noViewFoundException ->
@@ -387,4 +437,16 @@ class MountsFragmentTest {
                 val adapter = recyclerView.adapter
                 assertThat(adapter!!.itemCount, `is`(expectedCount))
             }
+
+    private fun setRefreshing() = object : ViewAction {
+        override fun getConstraints(): Matcher<View> = isAssignableFrom(SwipeRefreshLayout::class.java)
+
+        override fun getDescription(): String = "Set SwipeRefreshLayout refreshing state"
+
+        override fun perform(uiController: UiController, view: View) {
+            uiController.loopMainThreadUntilIdle()
+            val swipeRefreshLayout = view as SwipeRefreshLayout
+            swipeRefreshLayout.isRefreshing = true
+        }
+    }
 }
